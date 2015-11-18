abcfdf35bacc129afdb2a24d0514156ea6d4aeef
aumiaik7 <aumi.aik7@gmail.com>
2015-11-18 00:44:09 -0700

    added some cpp files

---
+++ cpp/valder.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include "valder.h"
+using namespace std;
+ 
+ 
+int valder::add(vector<double> a,vector<double> b)
+{
+  cout<< "Whazzaaaapp"<<endl;
+   return 2;
+}
+/*bool Time :: equals(const Time &otherTime)
+{
+     if(hour == otherTime.hour 
+          && minute == otherTime.minute 
+          && second == otherTime.second)
+          return true;
+     else
+          return false;
+	  }*/
